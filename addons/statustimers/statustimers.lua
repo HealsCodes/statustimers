86407b8a6e353b335e0de0a2540fe4254f27ec63
ThornyFFXI <10786796+ThornyFFXI@users.noreply.github.com>
2026-01-31 07:20:28 -0500

    4.1.6 - Replace deprecated SetWindowFontScale calls for Ashita 4.30+

--- addons/statustimers/statustimers.lua
+++ addons/statustimers/statustimers.lua
@@ -19,7 +19,7 @@
 
 addon.name    = 'statustimers';
 addon.author  = 'heals';
-addon.version = '4.1.503';
+addon.version = '4.1.6';
 addon.desc    = 'Replacement for the default status timer display';
 addon.link    = 'https://github.com/HealsCodes/statustimers';
 
