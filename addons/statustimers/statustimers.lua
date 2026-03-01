89e2c0abd4baa6936d0ea1fce879fa275d9c3847
Renée Köcher <heals@ashitaxi.heals-draws.art>
2026-03-01 15:25:25 +0100

    Bump version to 4.3.0202

--- addons/statustimers/statustimers.lua
+++ addons/statustimers/statustimers.lua
@@ -19,7 +19,7 @@
 
 addon.name    = 'statustimers';
 addon.author  = 'heals';
-addon.version = '4.3.0201';
+addon.version = '4.3.0202';
 addon.desc    = 'Replacement for the default status timer display';
 addon.link    = 'https://github.com/HealsCodes/statustimers';
 
